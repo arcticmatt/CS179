5b6a28f4805155ad7584c033b2344b5b939fc07e
Matt Lim <mlimcollege@gmail.com>
2016-04-11 13:36:05 -0700

    Fill in optimized transpose, do more README

--- labs/lab2/transpose_device.cu
+++ labs/lab2/transpose_device.cu
@@ -54,6 +54,35 @@ void naiveTransposeKernel(const float *input, float *output, int n) {
         output[j + n * i] = input[i + n * j];
 }
 
+/*
+ * The main optimization used here is the usage of shared memory. By using
+ * shared memory, we are able to get rid of the coalesced write, as seen
+ * above in the naive implemention. We also pad the shared memory (make it
+ * unnecessarily large) in order to avoid bank conflicts.
+ *
+ * We statically allocate a 65 x 64 chunk of shared memory. Only 64 x 64
+ * of this is used; the remaining row is the padding. In the first for-loop,
+ * we copy the 64 x 64 global memory chunk that the block is handling
+ * into our shared memory array. To ensure that we do not proceed any further
+ * until every thread in the block has copied its elements to shared memory,
+ * we call __syncthreads(). We then reinitialize/change some variables. Note
+ * that blockIdx.y and blockIdx.x get switched around for i0 and j0. This
+ * is explained further in the comments below, but in short it is because
+ * the transpose of some elements is in a different block than the original
+ * block. Then, in the last for loop, we write to output from shared memory.
+ * Note that we access output in a coalesced manner. We accessing
+ * the shared memory in a non-sequential manner; however, this does not
+ * affect performance. This is the benefit that shared memory gives us.
+ * Also note how the indices are switched in the write to output (we access
+ * different elements of sh_in and output). This, along with the block
+ * switching, is the transpose logic.
+ *
+ * Finally, note that, for threads in a warp, we read from shared memory
+ * in offsets of 65. This gives us a stride of 1 and 0 bank conflicts.
+ * More specifically, this ensures that when reading from the shared output,
+ * each read will access a separate bank. When writing to shared memory,
+ * we have sequential access within a warp, so bank conflicts are not a problem.
+ */
 __global__
 void shmemTransposeKernel(const float *input, float *output, int n) {
     // DONE: Modify transpose kernel to use shared memory. All global memory
@@ -64,6 +93,7 @@ void shmemTransposeKernel(const float *input, float *output, int n) {
     // 65 x 64 shared memory matrix.
     __shared__ float sh_in[65 * 64];
 
+    // Initial variable initialization.
     int i0 = threadIdx.x + 64 * blockIdx.x;
     int j0 = 4 * threadIdx.y + 64 * blockIdx.y;
     const int i1 = threadIdx.x;
@@ -98,18 +128,47 @@ void shmemTransposeKernel(const float *input, float *output, int n) {
     }
 }
 
+/*
+ * Besides the shared memory and padding as seen in shmemTransposeKernel,
+ * here are some other performance optimizations (mostly small ones) used
+ * in this method:
+ * - unroll the for-loops
+ * - get rid of unneccessary variables and reassignments (e.g. end_j0)
+ * - minimize instruction dependencies
+ */
 __global__
 void optimalTransposeKernel(const float *input, float *output, int n) {
     // TODO: This should be based off of your shmemTransposeKernel.
     // Use any optimization tricks discussed so far to improve performance.
     // Consider ILP and loop unrolling.
 
-    const int i = threadIdx.x + 64 * blockIdx.x;
-    int j = 4 * threadIdx.y + 64 * blockIdx.y;
-    const int end_j = j + 4;
+    // 65 x 64 shared memory matrix.
+    __shared__ float sh_in[65 * 64];
 
-    for (; j < end_j; j++)
-        output[j + n * i] = input[i + n * j];
+    // Initial variable initialization.
+    int i0 = threadIdx.x + 64 * blockIdx.x;
+    int j0 = 4 * threadIdx.y + 64 * blockIdx.y;
+    const int i1 = threadIdx.x;
+    int j1 = 4 * threadIdx.y;
+
+    // Unroll the first for-loop, which writes to shared memory.
+    sh_in[i1 + 65 * j1] = input[i0 + n * j0];
+    sh_in[i1 + 65 * (j1 + 1)] = input[i0 + n * (j0 + 1)];
+    sh_in[i1 + 65 * (j1 + 2)] = input[i0 + n * (j0 + 2)];
+    sh_in[i1 + 65 * (j1 + 3)] = input[i0 + n * (j0 + 3)];
+
+    // Make sure the entire block has writen to shared memory.
+    __syncthreads();
+
+    // Flip the block indices.
+    i0 = threadIdx.x + 64 * blockIdx.y;
+    j0 = 4 * threadIdx.y + 64 * blockIdx.x;
+
+    // Unroll the second for-loop, which writes to output.
+    output[i0 + n * j0] = sh_in[j1 + 65 * i1];
+    output[i0 + n * (j0 + 1)] = sh_in[(j1 + 1) + 65 * i1];
+    output[i0 + n * (j0 + 2)] = sh_in[(j1 + 2) + 65 * i1];
+    output[i0 + n * (j0 + 3)] = sh_in[(j1 + 3) + 65 * i1];
 }
 
 void cudaTranspose(
