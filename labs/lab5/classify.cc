b301eabb0ea16b0a0c214b83379b192e780094df
Matt Lim <mlimcollege@gmail.com>
2016-05-04 00:29:09 -0700

    Get everything working

--- labs/lab5/classify.cc
+++ labs/lab5/classify.cc
@@ -14,6 +14,8 @@
 
 using namespace std;
 
+static const float STEP_SIZE = 0.1;
+
 /*
 NOTE: You can use this macro to easily check cuda error codes
 and get more information.
@@ -80,27 +82,100 @@ void readLSAReview(string review_str, float *output, int stride) {
 }
 
 void classify(istream& in_stream, int batch_size) {
-    // TODO: randomly initialize weights. allocate and initialize buffers on
-    //       host & device. allocate and initialize streams
+    // Allocate host memory, initialize device memory.
+    size_t size_weights = REVIEW_DIM * sizeof(float);
+    size_t size_data = batch_size * (REVIEW_DIM + 1) * sizeof(float);
+    float *weights_host = (float *) malloc(size_weights);
+    float *weights_dev;
+    float *data_host1 = (float *) malloc(size_data);
+    float *data_dev1;
+    float *data_host2 = (float *) malloc(size_data);
+    float *data_dev2;
 
-    // main loop to process input lines (each line corresponds to a review)
+    // Allocate device memory.
+    gpuErrChk(cudaMalloc((void **) &weights_dev, size_weights));
+    gpuErrChk(cudaMalloc((void **) &data_dev1, size_data));
+    gpuErrChk(cudaMalloc((void **) &data_dev2, size_data));
+
+    // Fill the host weights with standard normal data.
+    gaussianFill(weights_host, REVIEW_DIM);
+
+    // Copy host memory to device memory.
+    gpuErrChk(cudaMemcpy(weights_dev, weights_host, size_weights,
+                cudaMemcpyHostToDevice));
+
+    // Create the stream.
+    cudaStream_t s[2];
+    cudaStreamCreate(&s[0]);
+    cudaStreamCreate(&s[1]);
+
+    // Create data in/out arrays
+    float *data_devs[2];
+    float *data_hosts[2];
+    data_devs[0] = data_dev1;
+    data_devs[1] = data_dev2;
+    data_hosts[0] = data_host1;
+    data_hosts[1] = data_host2;
+
+    // Main loop to process input lines (each line corresponds to a review).
     int review_idx = 0;
+    // Flag that determines which stream we will use.
+    int stream_flag = 0;
+    int lsa_offset = REVIEW_DIM + 1;
     for (string review_str; getline(in_stream, review_str); review_idx++) {
-        // TODO: process review_str with readLSAReview
+        float *data_dev = data_devs[stream_flag];
+        float *data_host = data_hosts[stream_flag];
 
-        // TODO: if you have filled up a batch, copy H->D, call kernel and copy
-        //      D->H all in a stream
+        readLSAReview(review_str,
+                data_host + review_idx * lsa_offset, 1);
+
+        // Check if full batch. If so, run kernel.
+        if (review_idx == batch_size - 1) {
+            review_idx = -1;
+            //for (int i = 0; i < batch_size; i++) {
+                //printf("data_host[%d] = %f\n", i, data_host[i]);
+            //}
+            // Host to device.
+            gpuErrChk(cudaMemcpyAsync(data_dev, data_host, size_data,
+                        cudaMemcpyHostToDevice, s[stream_flag]));
+            // Synchronize streams, so that kernels do not run in parallel.
+            // Needed so that weights get updated batch-wise.
+            gpuErrChk(cudaStreamSynchronize(s[stream_flag ^ 1]));
+            // Run kernel.
+            float error = cudaClassify(data_dev, batch_size, STEP_SIZE,
+                    weights_dev, s[stream_flag]);
+            printf("Error = %f\n", error);
+            // Change streams.
+            stream_flag ^= 1;
+        }
     }
 
-    // TODO: print out weights
-    // TODO: free all memory
+    // Copy device weights to host.
+    gpuErrChk(cudaMemcpy(weights_host, weights_dev, size_weights,
+                cudaMemcpyDeviceToHost));
+    for (int i = 0; i < REVIEW_DIM; i++) {
+        printf("Weight #%d = %f\n", i, weights_host[i]);
+    }
+
+    // Destroy streams.
+    cudaStreamDestroy(s[0]);
+    cudaStreamDestroy(s[1]);
+
+    // Free memory on host and device.
+    free(weights_host);
+    free(data_host1);
+    free(data_host2);
+
+    gpuErrChk(cudaFree(weights_dev));
+    gpuErrChk(cudaFree(data_dev1));
+    gpuErrChk(cudaFree(data_dev2));
 }
 
 int main(int argc, char** argv) {
     if (argc != 2) {
 		printf("./classify <path to datafile>\n");
 		return -1;
-    } 
+    }
     // These functions allow you to select the least utilized GPU
     // on your system as well as enforce a time limit on program execution.
     // Please leave these enabled as a courtesy to your fellow classmates
@@ -109,23 +184,23 @@ int main(int argc, char** argv) {
     TA_Utilities::select_least_utilized_GPU();
     int max_time_allowed_in_seconds = 100;
     TA_Utilities::enforce_time_limit(max_time_allowed_in_seconds);
-	
+
     // Init timing
 	float time_initial, time_final;
-	
+
     int batch_size = 2048;
-	
+
 	// begin timer
 	time_initial = clock();
-	
+
     ifstream ifs(argv[1]);
     stringstream buffer;
     buffer << ifs.rdbuf();
     classify(buffer, batch_size);
-	
+
 	// End timer
 	time_final = clock();
 	printf("Total time to run classify: %f (s)\n", (time_final - time_initial) / CLOCKS_PER_SEC);
-	
+
 
 }
--- labs/lab5/classify_cuda.cu
+++ labs/lab5/classify_cuda.cu
@@ -21,11 +21,79 @@ __global__
 void trainLogRegKernel(
     float *data,
     int batch_size,
-    int step_size,
+    float step_size,
 	float *weights,
     float *errors)
 {
-    // TODO: write me
+    // Shared memory whose size is determined by cudaClassify.
+    // Shared memory holds the weights and the gradient.
+    extern __shared__ float sdata[];
+    float *sdata_weights = sdata;
+    float *sdata_grad = sdata + REVIEW_DIM;
+
+    // Use index within the block to populate shared memory.
+    unsigned int block_index = threadIdx.x;
+
+    while (block_index < REVIEW_DIM) {
+        // Populate shared memory.
+        sdata_weights[block_index] = weights[block_index];
+        sdata_grad[block_index] = 0;
+        block_index += blockDim.x;
+    }
+
+    // Make sure shared data has been fully populated.
+    __syncthreads();
+
+    // Now use the global thread index to calculate the gradient.
+    unsigned int original_thread_index = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int thread_index = original_thread_index;
+
+    while (thread_index < batch_size) {
+        // For every point in data, calculate the contribution to the gradient.
+        // This is given by
+        // (y_n * x_n) / (1 + exp(y_n w^T x_n)
+
+        // Pointer to the features of the current point.
+        float *curr_data = data + (thread_index * (REVIEW_DIM + 1));
+        float dot_prod = 0;
+        float y_val = curr_data[REVIEW_DIM];
+
+        // Calculate dot product.
+        for (int i = 0; i < REVIEW_DIM; i++) {
+            dot_prod += sdata_weights[i] * curr_data[i];
+        }
+
+        // Check for misclassified points.
+        bool sign_dot_prod = dot_prod > 0 ? true : false;
+        bool sign_y_val = y_val > 0 ? true : false;
+        if (sign_dot_prod != sign_y_val) {
+            atomicAdd(errors, 1);
+        }
+
+        // Update gradient.
+        for (int i = 0; i < REVIEW_DIM; i++) {
+            float grad_update = (y_val * curr_data[i]) / (1.0 + exp(y_val * dot_prod));
+            atomicAdd(&sdata_grad[i], grad_update);
+        }
+
+        thread_index += blockDim.x * gridDim.x;
+    }
+
+    // Make sure all threads in the block have updated the gradient.
+    __syncthreads();
+
+    if (threadIdx.x == 0) {
+        // Update global weights, taking into account all blocks.
+        for (int i = 0; i < REVIEW_DIM; i++) {
+            // Multiply gradient by (-1 / N) here, which flips the sign of
+            // the update to an addition.
+            atomicAdd(&weights[i], step_size * sdata_grad[i] * (1.0 / batch_size));
+        }
+    }
+
+    if (original_thread_index == 0) {
+        *errors /= (float) batch_size;
+    }
 }
 
 /*
@@ -35,16 +103,16 @@ void trainLogRegKernel(
  */
 float cudaClassify(
     float *data,
-    int batch_size, 
+    int batch_size,
     float step_size,
-    float *weights, 
+    float *weights,
     cudaStream_t stream)
 {
     int block_size = (batch_size < 1024) ? batch_size : 1024;
 
     // grid_size = CEIL(batch_size / block_size)
     int grid_size = (batch_size + block_size - 1) / block_size;
-    int shmem_bytes = 0;
+    int shmem_bytes = (REVIEW_DIM * 2) * sizeof(float);
 
     float *d_errors;
     cudaMalloc(&d_errors, sizeof(float));
